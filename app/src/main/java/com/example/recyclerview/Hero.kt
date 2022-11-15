b1528cd86022e761183e8059036101eca430d38b
sittisafiatun <safiatunsitti2020@gmail.com>
2022-11-15 12:33:35 +0700

    mpr

---
+++ app/src/main/java/com/example/recyclerview/Hero.kt
@@ -0,0 +1,13 @@
+package com.example.recyclerview
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+
+    @Parcelize
+    data class Hero(
+        var name: String,
+        var description: String,
+        var photo: Int
+    ) : Parcelable
+
